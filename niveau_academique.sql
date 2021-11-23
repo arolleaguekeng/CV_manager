0714f457ba54f9641794036f9d77ea956075471c
azangue-cmd <83842067+azangue-cmd@users.noreply.github.com>
2021-11-23 14:04:32 +0100

    Delete niveau_academique.sql

--- niveau_academique.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABBLE niveau_academique
-(
-    id_niveau INT PRIMARY KEY NOT NULL,
-    nom_niveau VARCHAR (100),
-    Ddepartementid_departement INT,
-    coursfiliereid_filiere INT,
-    coursniveau_academique_niveau INT,
-    coursid_cours INT
-)
