6a39a0d81b3f38a82e6641c279ac58ce6eca517f
arolleaguekeng <93347411+arolleaguekeng@users.noreply.github.com>
2021-11-24 12:04:57 -0800

    Delete salle_normale.sql

--- salle_normale.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE salle_normale
-(
-    id INT PRIMARY KEY NOT NULL,
-    nombre_place INT,
-    nombre_etudiant INT,
-    nom_salle_normale VARCHAR(255),
-    cycle_diplome INT,
-    periode_academique INT
-);
