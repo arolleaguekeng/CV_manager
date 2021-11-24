594bf83765c63cae0cba9be497d4d33d67c74d2b
arolleaguekeng <93347411+arolleaguekeng@users.noreply.github.com>
2021-11-24 12:02:49 -0800

    Delete laboratoires.sql

--- laboratoires.sql
+++
@@ -1,8 +0,0 @@
-    CREATE TABLE laboratoires 
-    (
-         id_laboratoire INT PRIMARY KEY NOT NULL,
-         type_laboratoire VARCHAR (100),
-         max_nombre_etudiant INT, 
-         min_nombre_etudiant INT,
-         salle_de_cours INT 
-    );
