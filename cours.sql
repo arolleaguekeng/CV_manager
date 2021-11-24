a2e4f5b399f0668f41e08a936b70b3df79aa6f93
arolleaguekeng <93347411+arolleaguekeng@users.noreply.github.com>
2021-11-24 11:58:44 -0800

    Delete cours.sql

--- cours.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE cours(
-   filiereid_filiere int primary key not null,
-   niveau_academiqueid_niveau int primary key not null,
-   id_cours int primary key not null,
-   niveau_academiqueid_niveau2 int
- );
