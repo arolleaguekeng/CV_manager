014acc81583acc29ff4899a6c1b6f252d679b4ad
arolleaguekeng <arolleaguekeng@gmail.com>
2021-12-09 12:24:42 +0100

    id auto increment

--- cvm_cre.sql
+++ cvm_cre.sql
@@ -179,12 +179,12 @@ CREATE TABLE pdf(
 --
 -- create table file/added date by AGUEKENG
 --
-/*
+
 CREATE TABLE info_add  
 (
     info_add_id int  PRIMARY KEY,
     file_id  INT  NOT NULL
-);*/
+);
 
 
 
