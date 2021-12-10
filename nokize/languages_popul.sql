0613441e9b5c7a14aa1cb2d5751523e854e606e0
nokizemaxime <93329117+nokizemaxime@users.noreply.github.com>
2021-12-10 07:16:24 +0100

    Add files via upload

---
+++ nokize/languages_popul.sql
@@ -0,0 +1,21 @@
+--insert datas on tables by Nokize
+
+INSERT INTO languages(language_id,usersuser_id,language_level,language_name)values(01,001,'Normal','Francais');
+INSERT INTO languages(language_id,usersuser_id,language_level,language_name)values(02,001,'Moyen','Anglais');
+
+
+INSERT INTO languages(language_id,usersuser_id,language_level,language_name)values(01,002,' Normal','Francais');
+INSERT INTO languages(language_id,usersuser_id,language_level,language_name)values(02,002,'Normal','Anglais');
+
+INSERT INTO languages(language_id,usersuser_id,language_level,language_name)values(01,003,' Moyen','Francais');
+INSERT INTO languages(language_id,usersuser_id,language_level,language_name)values(02,003,'Superieur','Anglais');
+
+INSERT INTO languages(language_id,usersuser_id,language_level,language_name)values(01,004,' Normal','Francais');
+INSERT INTO languages(language_id,usersuser_id,language_level,language_name)values(03,004,'Normal','Espagnol');
+
+INSERT INTO languages(language_id,usersuser_id,language_level,language_name)values(02,005,' Normal','Anglais');
+INSERT INTO languages(language_id,usersuser_id,language_level,language_name)values(04,005,'Normal','madanrin');
+
+INSERT INTO languages(language_id,usersuser_id,language_level,language_name)values(02,006,' Normal','Anglais');
+
+INSERT INTO languages(language_id,usersuser_id,language_level,language_name)values(04,007,'Normal','madanrin');
---
+++ nokize/profiles_popul.sql
@@ -0,0 +1,11 @@
+--insert datas on tables by Nokize
+
+INSERT INTO profiles(profile_id,usersuser_id,first_name,last_name,born_date,phone_number)values(01,001,"nokize","maxime","01/01/2000",692400485);
+					 
+INSERT INTO profiles(profile_id,usersuser_id,first_name,last_name,born_date,phone_number)values(02,002,"kaze","cedric","05/07/2001",654347689);
+					 
+INSERT INTO profiles(profile_id,usersuser_id,first_name,last_name,born_date,phone_number)values(03,003,"aguekeng","arole","24/03/2002",693456178);
+					
+INSERT INTO profiles(profile_id,usersuser_id,first_name,last_name,born_date,phone_number)values(04,004,"ntentie","diovani","07/05/1999",656004895);
+					
+INSERT INTO profiles(profile_id,usersuser_id,first_name,last_name,born_date,phone_number)values(05,005,"yianyi","stefane","19/08/2000",699456785);
