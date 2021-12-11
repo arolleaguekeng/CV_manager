c9e51b2dedd0c989b84a3bec6a8dee15268c2b2b
donit <donit@DESKTOP-LV7RBPD>
2021-12-11 03:27:52 +0100

    creation fonctionnallté dans la table compétence

--- features/competences/add_competences.sql
+++ features/competences/add_competences.sql
@@ -1,3 +1,3 @@
 
 INSERT INTO competences(competence_id,useruser_id,undercompetence_id,intitule)
-VALUES (&competence_id,'&useruser_id','&undercompetence_id','&intitule');
+VALUES (&competence_id,'&useruser_id','&undercompetence_id ','&intitule');
